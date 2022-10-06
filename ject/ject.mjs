339d176e7d2f8ae7d5c7fd7d4d7486aa4dcef992
Никита Яхин <mr.y.nikita@gmail.com>
2022-10-07 01:57:20 +0500

    0.2.0
    Изменения:
    * Добавление раздела приёмов в документации.
    * Добавлен приём умного заполнения.
    * Добавлен приём глубокого заполнения.
    * Добавлены функциональные цепочки глубокого заполнения.
    * Изменена документация.
    * Изменен манифест пакета.

--- ject/ject.mjs
+++ ject/ject.mjs
@@ -82,6 +82,371 @@ export function jectFill(t) {
 };
 
 //#endregion
+//#region change 0.0.0
+
+/**
+ * @typedef TBchange
+ * @prop {{}} ject
+ * @prop {[{}]} changes
+ * @typedef {TBchange} Tchange
+*/
+
+/** @param {Tchange} t */
+function changeDeceit(t) {
+
+    try {
+
+        return changeVerify(t);
+
+    } catch (e) {
+
+        if (config.strict) throw e;
+
+        return undefined;
+
+    };
+
+};
+/** @param {Tchange} t */
+function changeVerify(t) {
+
+    const {
+
+
+
+    } = t;
+
+    return changeHandle(t);
+
+};
+/** @param {Tchange} t */
+function changeHandle(t) {
+
+    let {
+
+
+
+    } = t;
+
+
+
+    t = {
+
+        ...t,
+
+    };
+
+    return changeComply(t);
+
+};
+/** @param {Tchange} t */
+function changeComply(t) {
+
+    const {
+
+        ject,
+        changes,
+
+    } = t;
+
+    changes.forEach(c => Object.entries(c).forEach(p => ject[p[0]] = p[1]));
+
+};
+
+/**
+ * Функция для изменения исходного объекта свойствами остаточных объектов.
+ * 
+ * Изменение происходит за счет свойств остаточных объектов.
+ * Если свойство есть у остаточного объекта, то его значение будет перезаписано исходному.
+ * @param {{}} ject Исходный объект.
+ * @param {...{}} changes Остаточные объекты изменения.
+ * - Версия `0.0.0`
+ * - Цепочка `DVHCa`
+*/
+export function jectChange(ject, ...changes) {
+
+    changeDeceit({ ject, changes, });
+
+};
+
+//#endregion
+//#region getDeep 0.0.0
+
+/**
+ * @typedef TBgetDeep
+ * @prop {{}} ject
+ * @prop {string} property
+ * @typedef {TBgetDeep} TgetDeep
+*/
+
+/** @param {TgetDeep} t */
+function getDeepDeceit(t) {
+
+    try {
+
+        return getDeepVerify(t);
+
+    } catch (e) {
+
+        if (config.strict) throw e;
+
+        return undefined;
+
+    };
+
+};
+/** @param {TgetDeep} t */
+function getDeepVerify(t) {
+
+    const {
+
+
+
+    } = t;
+
+    return getDeepHandle(t);
+
+};
+/** @param {TgetDeep} t */
+function getDeepHandle(t) {
+
+    let {
+
+
+
+    } = t;
+
+    t = {
+
+        ...t,
+
+    };
+
+    return getDeepComply(t);
+
+};
+/** @param {TgetDeep} t */
+function getDeepComply(t) {
+
+    const {
+
+        ject,
+        property,
+
+    } = t;
+
+    const structures = [ject];
+
+    for (; structures.length;) {
+
+        const s = structures.pop();
+
+        if (s.hasOwnProperty(property)) return s[property];
+        else for (const v of Object.values(s)) if (typeof v === 'object') structures.push(v);
+
+    };
+
+};
+
+/**
+ * Функция для извлечения вложенного свойства из объекта.
+ * - Версия `0.0.0`
+ * - Цепочка `DVHCa`
+*/
+export function jectGetDeep(ject, property) {
+
+    return getDeepDeceit({ ject, property });
+
+};
+
+//#endregion
+//#region setDeep 0.0.0
+
+/**
+ * @typedef TBsetDeep
+ * @prop {any} value
+ * @typedef {TBsetDeep&TgetDeep} TsetDeep
+*/
+
+/** @param {TsetDeep} t */
+function setDeepDeceit(t) {
+
+    try {
+
+        return setDeepVerify(t);
+
+    } catch (e) {
+
+        if (config.strict) throw e;
+
+        return undefined;
+
+    };
+
+};
+/** @param {TsetDeep} t */
+function setDeepVerify(t) {
+
+    const {
+
+
+
+    } = t;
+
+    return setDeepHandle(t);
+
+};
+/** @param {TsetDeep} t */
+function setDeepHandle(t) {
+
+    let {
+
+
+
+    } = t;
+
+    t = {
+
+        ...t,
+
+    };
+
+    return setDeepComply(t);
+
+};
+/** @param {TsetDeep} t */
+function setDeepComply(t) {
+
+    const {
+
+        ject,
+        value,
+        property,
+
+    } = t;
+
+    const structures = [ject];
+
+    for (; structures.length;) {
+
+        const s = structures.pop();
+
+        if (s.hasOwnProperty(property)) s[property] = value;
+        else for (const v of Object.values(s)) if (typeof v === 'object') structures.push(v);
+
+    };
+
+};
+
+/**
+ * Функция для глубокой установки вложенного свойства.
+ * - Версия `0.0.0`
+ * - Цепочка `DVHCa`
+ * @param {{}} ject Объект глубокой установки.
+ * @param {any} value Значение для свойства глубокой установки.
+ * @param {string} property Свойство глубокой установки.
+ * @return {void}
+*/
+export function jectSetDeep(ject, property, value) {
+
+    setDeepDeceit({ ject, property, value });
+
+};
+
+//#endregion
+//#region supplement 0.0.0
+
+/**
+ * @typedef TBsupplement
+ * @prop {{}} ject
+ * @prop {[{}]} supplements
+ * @typedef {TBsupplement} Tsupplement
+*/
+
+/** @param {Tsupplement} t */
+function supplementDeceit(t) {
+
+    try {
+
+        return supplementVerify(t);
+
+    } catch (e) {
+
+        if (config.strict) throw e;
+
+        return undefined;
+
+    };
+
+};
+/** @param {Tsupplement} t */
+function supplementVerify(t) {
+
+    const {
+
+
+
+    } = t;
+
+    return supplementHandle(t);
+
+};
+/** @param {Tsupplement} t */
+function supplementHandle(t) {
+
+    let {
+
+
+
+    } = t;
+
+
+
+    t = {
+
+        ...t,
+
+    };
+
+    return supplementComply(t);
+
+};
+/** @param {Tsupplement} t */
+function supplementComply(t) {
+
+    const {
+
+        ject,
+        supplements,
+
+    } = t;
+
+    supplements.forEach(s => {
+
+        Object.entries(s).forEach(p => {
+
+            if (!ject.hasOwnProperty(p[0])) ject[p[0]] = p[1];
+
+        });
+
+    });
+
+};
+
+/**
+ * Функция дополнения объекта недостоющими свойствами указанных объектов.
+ * - Версия `0.0.0`
+ * - Цепочка `DVHCa`
+ * @param {{}} ject Исходный объект.
+ * @param {...{}} supplements Объекты дополнения.
+*/
+export function jectSupplement(ject, ...supplements) {
+
+    supplementDeceit({ ject, supplements });
+
+};
+
+//#endregion
 
 /**
  * @file ject.mjs
